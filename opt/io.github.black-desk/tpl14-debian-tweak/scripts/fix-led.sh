65f98bcb010929fb8eeacdbe2105ee35327b9b0d
black-desk <me@black-desk.cn>
2023-08-30 00:45:55 +0800

    fix: correct capslock led

---
+++ opt/io.github.black-desk/tpl14-debian-tweak/scripts/fix-led.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Some keyboard manufacture will
+# make capslock key switch the LED state
+# without kernel say it to.
+# This make user like me who swap capslock with other keys panic.
+#
+# This script is used to keep capslock LED on builtin keyboard
+# to be in correct state.
+
+set -e
+set -o pipefail
+
+LED=${LED:=/sys/class/leds/input0::capslock}
+EVENT=${EVENT:=/dev/input/event0}
+
+fix_led() {
+	correct_state=$(cat "$LED/brightness")
+	wrong_state=0
+	[[ "$correct_state" == 0 ]] && wrong_state=1
+
+	{
+		echo "$wrong_state"
+		echo "$correct_state"
+	} >"$LED/brightness"
+}
+
+while read -r line; do
+	[[ "$line" == *"code 1 (KEY_ESC), value 0"* ]] &&
+		fix_led
+done < <(evtest "$EVENT")
