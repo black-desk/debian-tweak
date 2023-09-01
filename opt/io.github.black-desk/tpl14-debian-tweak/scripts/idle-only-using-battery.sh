6ee9d65c0df1d08957628e3de4ee3e36a919672b
black-desk <me@black-desk.cn>
2023-09-01 08:31:36 +0800

    chore: add a log in idle-on-using-battery.sh

--- opt/io.github.black-desk/tpl14-debian-tweak/scripts/idle-only-using-battery.sh
+++ opt/io.github.black-desk/tpl14-debian-tweak/scripts/idle-only-using-battery.sh
@@ -9,6 +9,13 @@ function update_idle_delay() {
 	idle_delay=0
 	[ "$status" == "true" ] && idle_delay=120
 
+	old_idle_delay=$(gsettings get org.gnome.desktop.session idle-delay | awk '{print $2}' )
+
+	if [[ "$old_idle_delay" == "$idle_delay" ]]; then
+		return
+	fi
+
+	echo "Update idle_delay from $old_idle_delay to $idle_delay"
 	gsettings set org.gnome.desktop.session idle-delay "$idle_delay"
 }
 
