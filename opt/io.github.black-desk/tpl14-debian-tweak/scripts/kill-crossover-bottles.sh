742a4cf805484464bff03b3728d3b75fb0c42def
black-desk <me@black-desk.cn>
2023-09-02 17:27:30 +0800

    feat: add kill-crossover-bottles.sh

---
+++ opt/io.github.black-desk/tpl14-debian-tweak/scripts/kill-crossover-bottles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+CX_ROOT=${CX_ROOT:=/opt/cxoffice}
+
+CX_BOTTLES_PATH=${CX_BOTTLES_PATH:="$HOME/.cxoffice/"}
+
+while read -r line; do
+	"$CX_ROOT"/bin/wine --bottle "$line" --wl-app wineboot.exe -- --force --kill || true
+done < <(
+	find "$CX_BOTTLES_PATH" -mindepth 1 -maxdepth 1 -type d \
+		! -name "tie" \
+		! -name "icons" \
+		! -name "installers" \
+		-printf %p\\n
+)
+
+echo "done"
